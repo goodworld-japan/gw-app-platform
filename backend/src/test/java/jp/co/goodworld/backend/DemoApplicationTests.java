bd91f60c7e82465d9ed66c942b1b9fd692d74e80
chin.shuji <chin.shuji@gmail.com>
2024-03-23 20:44:18 +0900

    プロジェクトの雛形の追加

---
+++ backend/src/test/java/jp/co/goodworld/backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.goodworld.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
